cc3a1bf35c6e8174b83961ff26b8dc9bcb4fca03
kudretkrbyk <kudretkrbyk@gmail.com>
2024-12-16 23:00:05 +0300

    added mainContent Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Components/Navbar";
 import AboutMe from "./Components/AboutMe";
+import TrendingTopics from "./Components/TrendingTopics";
 
 function App() {
   return (
     <div className="bg-[#fff4f5] w-full h-full">
       <Navbar></Navbar>
       <AboutMe></AboutMe>
+      <TrendingTopics></TrendingTopics>
     </div>
   );
 }
---
+++ src/Components/TrendingTopics.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function TrendingTopics() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full p-24 gap-5">
+      <div>
+        <h2 className="title bordered medium textcenter">
+          <svg className="icon large secondary-color">
+            <use xlink:href="https://themes.estudiopatagon.com/wordpress/zento-personal/wp-content/themes/zento/assets/images/svg-icons.svg#trending-icon"></use>
+          </svg>{" "}
+          Trending Topics
+        </h2>
+      </div>
+      <div className="bg-white w-full h-24 rounded-full p-10">s</div>
+    </div>
+  );
+}
