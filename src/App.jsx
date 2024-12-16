01bbfbe8c20b0c4cc2f78a2fa5658be228ba9cda
kudretkrbyk <kudretkrbyk@gmail.com>
2024-12-16 22:43:12 +0300

    added aboutMe

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./Components/Navbar";
+import AboutMe from "./Components/AboutMe";
 
 function App() {
   return (
     <div className="bg-[#fff4f5] w-full h-full">
       <Navbar></Navbar>
+      <AboutMe></AboutMe>
     </div>
   );
 }
