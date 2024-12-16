cc7965283cafb7f2d2349a4ce4825398a5640b89
kudretkrbyk <kudretkrbyk@gmail.com>
2024-12-16 22:55:09 +0300

    added animation

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,6 +1,6 @@
 export default function AboutMe() {
   return (
-    <div className="flex items-center justify-center p-10">
+    <div className="flex items-center justify-center p-10 gap-10">
       <div className="text w-full flex flex-col items-start justify-center gap-4">
         <h1 className="title ularge fw-medium">
           Hey, I’m&nbsp;
@@ -16,12 +16,19 @@ export default function AboutMe() {
         <div className="w-full flex flex-col gap-2">
           <p>Let's Connect</p>
           <div className="bg-white border rounded flex items-center justify-between gap-2 w-full  p-1 h-10">
-            <input className=""></input>
+            <input className="focus:outline-none px-12 w-full"></input>
             <button className="w-2/12">Get started</button>
           </div>
         </div>
       </div>
-      <div className="w-full"></div>
+      <div className="w-full">
+        <div></div>
+        <img
+          className="w-[442px] h-full object-cover object-center animate-border-morph"
+          src="https://themes.estudiopatagon.com/wordpress/zento-personal/wp-content/uploads/2024/03/about-personal.webp"
+          alt="Morphing Image"
+        />
+      </div>
     </div>
   );
 }
