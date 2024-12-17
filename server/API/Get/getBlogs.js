2a019b6c31cc2f42c2a80a843c39b5b568ae7300
kudretkrbyk <kudretkrbyk@gmail.com>
2024-12-17 20:51:59 +0300

    added getBlogs with leftjoin

--- server/API/Get/getBlogs.js
+++ server/API/Get/getBlogs.js
@@ -2,10 +2,24 @@ const express = require("express");
 const pool = require("../../config/db");
 const router = express.Router();
 
-// Tüm blogları getir
+// Tüm blogları kategori adı ile getir
 router.get("/blogs", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM blog_posts");
+    const query = `
+      SELECT 
+        blog_posts.id, 
+        categories.kategori_adi AS kategori, 
+        blog_posts.baslik, 
+        blog_posts.foto1, 
+        blog_posts.foto2, 
+        blog_posts.metin1, 
+        blog_posts.metin2, 
+        blog_posts.created_at
+      FROM blog_posts
+      LEFT JOIN categories ON blog_posts.kategori_id = categories.id
+      ORDER BY blog_posts.created_at DESC
+    `;
+    const result = await pool.query(query);
     res.json(result.rows);
   } catch (error) {
     console.error(error.message);
@@ -13,13 +27,25 @@ router.get("/blogs", async (req, res) => {
   }
 });
 
-// Tek bir blogu ID'ye göre getir
+// Tek bir blogu kategori adı ile getir
 router.get("/blogs/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const result = await pool.query("SELECT * FROM blog_posts WHERE id = $1", [
-      id,
-    ]);
+    const query = `
+      SELECT 
+        blog_posts.id, 
+        categories.kategori_adi AS kategori, 
+        blog_posts.baslik, 
+        blog_posts.foto1, 
+        blog_posts.foto2, 
+        blog_posts.metin1, 
+        blog_posts.metin2, 
+        blog_posts.created_at
+      FROM blog_posts
+      LEFT JOIN categories ON blog_posts.kategori_id = categories.id
+      WHERE blog_posts.id = $1
+    `;
+    const result = await pool.query(query, [id]);
     if (result.rows.length === 0) return res.status(404).send("Blog not found");
     res.json(result.rows[0]);
   } catch (error) {
