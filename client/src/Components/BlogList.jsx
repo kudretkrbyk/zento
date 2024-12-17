914544e377e6ee388d4c806b629047f008f4b23d
kudretkrbyk <kudretkrbyk@gmail.com>
2024-12-17 23:07:07 +0300

    added style

--- client/src/Components/BlogList.jsx
+++ client/src/Components/BlogList.jsx
@@ -14,18 +14,25 @@ export default function BlogList() {
   }
 
   return (
-    <div className="w-full">
-      <h1 className="text-2xl font-bold mb-4">Blog Listesi</h1>
+    <div className="w-full flex flex-col items-start justify-center gap-10 p-10">
       {blogs.map((blog) => (
-        <div key={blog.id} className="border-b mb-4 pb-4">
-          <h2 className="text-xl font-semibold">{blog.baslik}</h2>
-          <p className="text-gray-600 mb-2">Kategori: {blog.kategori}</p>
+        <div
+          key={blog.id}
+          className="w-full flex items-start justify-center gap-5  bg-white p-4"
+        >
           <img
             src={blog.foto1}
             alt="Blog Görsel"
-            className="w-full h-48 object-cover rounded-lg mb-4"
+            className="w-48 h-48 object-cover rounded-lg mb-4"
           />
-          <p className="text-gray-700">{blog.metin1}</p>
+          <div>
+            {" "}
+            <h2 className="text-xl font-semibold">{blog.baslik}</h2>
+            <p className="text-gray-600 mb-2">Kategori: {blog.kategori}</p>
+            <p className="text-gray-700">
+              {blog.metin1.split(" ").slice(0, 20).join(" ")}...
+            </p>
+          </div>
         </div>
       ))}
     </div>
