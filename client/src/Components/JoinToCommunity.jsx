725bdd5fa1ed9f1e874a97aafb893bccc093e1fd
kudretkrbyk <kudretkrbyk@gmail.com>
2024-12-18 19:35:07 +0300

    added style

--- client/src/Components/JoinToCommunity.jsx
+++ client/src/Components/JoinToCommunity.jsx
@@ -1,14 +1,14 @@
 export default function JoinToCommunity() {
   return (
-    <div className="w-full h-60 bg-white flex items-start justify-between p-10">
-      <div>
+    <div className="w-full h-60 bg-white flex items-start justify-between p-20">
+      <div className="flex flex-col items-start justify-center gap-5">
         <h2 className="font-bold">Join to our community 👋</h2>
         <p>
           Unlock full access to&nbsp;<b>Zento</b>&nbsp;and see the entire
           library of&nbsp;<b>paid-members</b>&nbsp;only posts.
         </p>
       </div>
-      <div>
+      <div className="flex flex-col items-center justify-center gap-5">
         <p className=" text-center">
           Subscribe to our <strong>Newsletter</strong>, cancel at&nbsp;
           <b>anytime.</b>
