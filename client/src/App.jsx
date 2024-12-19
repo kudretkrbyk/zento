d67c086fd1b40f60b4c9dda5179d42ef2ca6ac90
kudretkrbyk <kudretkrbyk@gmail.com>
2024-12-19 20:34:58 +0300

    Login and signup pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,45 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import Main from "./Components/Pages/Main";
 import BlogDetail from "./Pages/BlogDetail";
+import LogIn from "./Components/Pages/LogIn";
+import SignUp from "./Components/Pages/SignUp";
 
 function App() {
   return (
     <Router>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/blogs/:id" element={<BlogDetail />} />
-      </Routes>
-      <Footer></Footer>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/logIn" element={<LogIn />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/blogs/:id" element={<BlogDetail />} />
+        </Routes>
+      </Layout>
     </Router>
+  );
+}
+
+function Layout({ children }) {
+  const location = useLocation();
+
+  // Navbar ve Footer'ın görünmeyeceği rotalar
+  const hiddenRoutes = ["/logIn", "/signUp"];
+
+  const shouldHide = hiddenRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {!shouldHide && <Navbar />}
+      {children}
+      {!shouldHide && <Footer />}
+    </>
   );
 }
 
--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -14,8 +14,12 @@ export default function Navbar() {
         <div className="w-44 h-24 bg-contain bg-no-repeat bg-center bg-[url(https://themes.estudiopatagon.com/wordpress/zento-personal/wp-content/uploads/2024/03/logo-zento-personal-1.png)] "></div>
         <div className="flex items-center justify-center gap-2">
           <div>
-            <button className=" p-2 px-4 text-white rounded ">Subscribe</button>
+            <button className=" p-2 px-4 ">Subscribe</button>
           </div>
+          <div>
+            <button className=" p-2 px-4 ">Log-In</button>
+          </div>
+
           <div>
             <TiThMenuOutline className="size-5" />
           </div>
